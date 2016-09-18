ef80e17476f4ae8831f94608a4100df2bc8faa26
Z <noneofyourbusiness.com>
2016-09-18 15:42:53 -0400

    formatting

--- main.py
+++ main.py
@@ -12,42 +12,44 @@ def constructEnsemble(models, parties):
             state = stateData["State"]
             
             if state not in ensemble:
-                ensemble[state] = {}
+                ensemble = addState(ensemble, state, parties)
 
-                for party in parties:
-                    ensemble[state]["{}{}".format(\
-                            "voteShare", party)] = 0                           
-                    ensemble[state]["{}{}".format(\
-                            "winProb", party)] = 0
-
-                ensemble[state]["numVoteShares"] = 0
-                ensemble[state]["numWinProbs"] = 0
-
-            voteSharesIsWhole = True
-            winProbsIsWhole = True
-
-            for party in parties:
-                voteShare = float(stateData["{}{}".format(\
-                        "VoteShare", party)])
-                winProb = float(stateData["{}{}".format(\
-                        "WinProb", party)])
-                voteSharesIsWhole = \
-                        voteSharesIsWhole and voteShare >= 0
-                winProbsIsWhole = \
-                        winProbsIsWhole and winProb >= 0
-
-                if voteShare >= 0:
-                    ensemble[state]["{}{}".format(\
-                            "voteShare", party)] += voteShare
-
-                if winProb >= 0:
-                    ensemble[state]["{}{}".format(\
-                            "winProb", party)] += winProb
-
-            ensemble[state]["numVoteShares"] += int(voteSharesIsWhole)
-            ensemble[state]["numWinProbs"] += int(winProbsIsWhole)
+            ensemble = addStateData(ensemble, parties, state, stateData)
 
     csvFile.close()
+    return ensemble
+
+def addState(ensemble, state, parties):
+    ensemble[state] = {}
+
+    for party in parties:
+        ensemble[state]["{}{}".format("voteShare", party)] = 0                           
+        ensemble[state]["{}{}".format("winProb", party)] = 0
+
+    ensemble[state]["numVoteShares"] = 0
+    ensemble[state]["numWinProbs"] = 0
+    return ensemble
+
+def addStateData(ensemble, parties, state, stateData):
+    voteSharesIsWhole = True
+    winProbsIsWhole = True
+
+    for party in parties:
+        voteShare = float(stateData["{}{}".format("VoteShare", party)])
+        winProb = float(stateData["{}{}".format("WinProb", party)])
+        voteSharesIsWhole = voteSharesIsWhole and voteShare >= 0
+        winProbsIsWhole = winProbsIsWhole and winProb >= 0
+
+        if voteShare >= 0:
+            ensemble[state]["{}{}".format("voteShare", party)] += \
+                    voteShare
+
+        if winProb >= 0:
+            ensemble[state]["{}{}".format("winProb", party)] += winProb
+
+    ensemble[state]["numVoteShares"] += int(voteSharesIsWhole)
+    ensemble[state]["numWinProbs"] += int(winProbsIsWhole)
+        
     return ensemble
 
 def processEnsemble(ensemble, parties):
