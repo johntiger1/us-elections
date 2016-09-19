ad8b295e2c768f4df18b01479da3023b32c76266
Z <noneofyourbusiness.com>
2016-09-19 08:15:10 -0400

    data

--- plot.py
+++ plot.py
@@ -3,21 +3,27 @@ import matplotlib.pyplot
 def plotEnsemble(ensembleDict):
     plt = matplotlib.pyplot
     fig, ax = plt.subplots()
-    xyData = constructXY(ensembleDict)
+    n = len(ensembleDict["parties"])
+    xyData = constructXY(ensembleDict, n)
     size = 200
     colorD = [x / 255 for x in [30, 144, 255]]
     colorR = [x / 255 for x in [220, 20, 60]]
-    scatterD = plt.scatter(xyData["voteSharesD"], xyData["winProbsD"], \
-            s=xyData["sizeD"], c=colorD, alpha=0.5, label="Democrat Ahead")
-    scatterR = plt.scatter(xyData["voteSharesR"], xyData["winProbsR"], \
-            s=xyData["sizeR"], c=colorR, alpha=0.5, label="Republican Ahead")
+    scatterD = plt.scatter(\
+            xyData["voteSharesD"], xyData["winProbsD"], \
+            s=xyData["sizeD"], c=colorD, alpha=0.5, \
+            label="Democrats Ahead")
+    scatterR = plt.scatter(
+            xyData["voteSharesR"], xyData["winProbsR"], \
+            s=xyData["sizeR"], c=colorR, alpha=0.5, \
+            label="Republicans Ahead")
     plt, ax = constructLabels(plt, ax)
     plt.legend(handles=[scatterD, scatterR], loc=4)
     plt.savefig("plot.png")
+    annotate(ax, ensembleDict, n)
+    plt.savefig("plotLabelled.png")
     plt.close(fig)
 
-def constructXY(ensembleDict):
-    n = len(ensembleDict["parties"])
+def constructXY(ensembleDict, n):
     sizeMult = 20
     xyData = {}
     xyData["voteSharesD"] = [ensembleDict["voteShares"][i] \
@@ -33,6 +39,17 @@ def constructXY(ensembleDict):
     xyData["sizeR"] = [sizeMult * ensembleDict["ev"][i] \
             for i in range(n) if ensembleDict["parties"][i] == "R"]
     return xyData
+
+def annotate(ax, ensembleDict, n):
+    for i in range(n):
+        state = ensembleDict["states"][i]
+        voteShare = ensembleDict["voteShares"][i]
+        winProb =  ensembleDict["winProbs"][i]
+
+        if ensembleDict["parties"][i] == "D":
+            ax.annotate(state, (voteShare, winProb))
+        else:
+            ax.annotate(state, (100 - voteShare, 100 - winProb))
 
 def constructLabels(plt, ax):
     plt.xlim([15, 85])
